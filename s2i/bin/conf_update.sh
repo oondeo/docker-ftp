96b85fb1d9538929c58cab79506a0cfd03d229e6
Amon-Ra <amon.raj@gmail.com>
2019-03-31 19:54:54 +0200

    Fix init

--- s2i/bin/conf_update.sh
+++ s2i/bin/conf_update.sh
@@ -8,8 +8,8 @@ while true
 do
     sleep 10
     #conf-php $PHP_INI_PATH $UNIT_CONTROL_SOCKET $UNIT_CONFIG_DIR
-    $STI_SCRIPTS_PATH/conf-ssh
-    pkill -HUP -F $PID_FILE || true
     inotifywait -e CREATE -r $CONFIG_DIR
+    $STI_SCRIPTS_PATH/conf-ssh
+    pkill -HUP -F $PID_FILE || true    
     #curl -X PUT --data-binary @$1--unix-socket $2 http://localhost/config
 done
